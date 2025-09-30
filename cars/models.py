b13eb19cf86169c36cf99aba318c57264ce943f2
SAMUEL LIMA SALES <90739934+SamuelLSales@users.noreply.github.com>
2025-09-30 00:15:57 -0300

    Add files via upload

--- cars/models.py
+++ cars/models.py
@@ -15,6 +15,7 @@ class Car(models.Model):
     brand = models.ForeignKey(Brand, on_delete=models.PROTECT, related_name='car_brand')
     factory = models.IntegerField(blank=True, null=True)
     model_year = models.IntegerField(blank=True, null=True)
+    plate = models.CharField(max_length=10, blank=True, null=True)
     value = models.FloatField(blank=True, null=True)
 
     def __str__(self):
