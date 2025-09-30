dadcee70a26377e2dbf7bc1fd4f98162b38fccea
Samuel Lima Sales <samuel.lima21287@gmail.com>
2025-09-29 23:40:04 -0300

    commit

--- cars/admin.py
+++ cars/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
-from cars.models import Car
+from cars.models import Car, Brand
 
+
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
 
 class CarAdmin(admin.ModelAdmin):
     list_display = ('id', 'model', 'brand', 'factory', 'model_year', 'value')
     search_fields = ('model', 'brand')
 
+
+
+admin.site.register(Brand, BrandAdmin)
 admin.site.register(Car, CarAdmin)
